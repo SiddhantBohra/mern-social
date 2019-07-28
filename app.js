a5527a6d3a5d028b976f8c1f89a52592e18e3d1b
Siddhant Bohra <abhisidds@gmail.com>
2019-07-28 17:02:25 +0530

    Added gitignore and updated app.js

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ const morgan = require('morgan')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const parser = require('body-parser')
+const port = process.env.PORT||8080
 dotenv.config()
 
-//
+//import routes
+
+
+// middleware
 app.use(parser.json())
+app.use(morgan('dev'))
+
+//db
+
+mongoose.connect(process.env.MONGO_DB_URI,{useNewUrlParser : true}, () =>{
+    console.log('DB Connected Successfully')
+}).catch(err =>{
+    console.log('Error Connecting to DB' + err.message)
+})
+
+//Port Listen
+app.listen(port, () =>{
+    console.log('Node JS Application Listening on' + `${port}`)
+}).catch(err =>{
+    console.log(err)
+})
