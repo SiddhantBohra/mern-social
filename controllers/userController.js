5e4ddc724eb715f8f5fa85cc3fde444903227a55
Siddhant Bohra <abhisidds@gmail.com>
2019-08-06 22:51:55 +0530

    Added PUT method to update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userSchema')
+const _ = require('lodash')
 const userById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
         if (err || !user) {
@@ -30,14 +31,32 @@ const allUsers = (req, res) => {
         }
     }).select("name email updated created")
 }
-const getUser = (req,res) =>{
+const getUser = (req, res) => {
     req.profile.hashed_password = undefined
     req.profile.salt = undefined
     return res.json(req.profile)
+}
+const updateUser = (req, res, next) => {
+    let user = req.profile
+    user = _.extend(user, req.body)
+    user.updated = Date.now()
+    user.save(err => {
+        if (err) {
+            return res.status(400).json({
+                error: 'You are not authorized to perform this action'
+            })
+        }
+        else {
+            user.hashed_password = undefined
+            user.salt = undefined
+            res.json({user})
+        }
+    })
 }
 module.exports = {
     userById,
     hasAuthorization,
     allUsers,
-    getUser
+    getUser,
+    updateUser
 }
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const {signup,signin,signout,requireSignIn} = require("../controllers/authController")
-const {userById,allUsers,getUser} = require("../controllers/userController")
+const {userById,allUsers,getUser,updateUser} = require("../controllers/userController")
 
 router.get("/users", allUsers)
 router.get('/user/:userId',requireSignIn,getUser)
+router.put('/user/:userId',requireSignIn,updateUser)
 router.param('userId', userById)
 
 module.exports = router
