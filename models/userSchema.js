d39c5762b3505a5f96f5e7eb98104e3cfc1885df
Siddhant Bohra <abhisidds@gmail.com>
2019-08-06 12:59:28 +0530

    Added SHA256 for encryption

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-
+const uuidv1 = require('uuid/v1')
+const crypto = require('crypto')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -12,15 +13,41 @@ const userSchema = new mongoose.Schema({
         trim: true,
         required: true
     },
-    hashed_password :{
-        type : String,
-        required : true
+    hashed_password: {
+        type: String,
+        required: true
     },
-    salt : String,
-    created : {
-        type : Date,
-        default : Date.now()
+    salt: String,
+    created: {
+        type: Date,
+        default: Date.now()
     },
-    updated : Date
+    updated: Date
 });
-module.exports = mongoose.model("User" , userSchema)
+
+//Virtual Field
+userSchema.virtual('password')
+    .set(password => {
+        this._password = password
+        this.salt = uuidv1()
+        this.hashed_password = this.encryptPassword(password)
+    })
+    .get(() => {
+        return this.password
+    })
+userSchema.methods = {
+    encryptPassword: password => {
+        if (!password) {
+            return ""
+        }
+        try {
+           return crypto.createHmac('sha256', secret)
+                .update(password)
+                .digest('hex');
+        }
+        catch{
+            return "";
+        }
+    }
+}
+module.exports = mongoose.model("User", userSchema)
